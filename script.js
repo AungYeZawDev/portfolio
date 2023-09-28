b4020e6b3b06bbce1973dd3cb444a4e5a9ad2f66
Aung Ye Zaw <ay.zaw@mit.com.mm>
2023-09-28 22:40:59 +0630

    initialization

---
+++ script.js
@@ -0,0 +1,15 @@
+// You can add JavaScript functionality here if needed
+// Example: Smooth scrolling to sections when clicking on navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        window.scrollTo({
+            top: targetElement.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
